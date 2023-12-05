8388f7d45d1acd44bfa5e42cc743ab687f64bc30
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-05 23:59:47 +0300

    Resolved conflict in 'feature-b'

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Открытие магазина ожидается через 4 дня");
+
+        System.out.println("Открытие магазина ожидается через 2 месяца");
     }
 }
