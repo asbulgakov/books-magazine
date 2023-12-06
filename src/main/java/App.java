423becac8da2b6eaaa16f901edf3343f603bc782
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-06 16:57:32 +0300

    Revert "Create branch 'feature-a' end add main class"
    This reverts commit 0d520bae327d1b3b13c4403e511cce22f2eefe32.

--- src/main/java/App.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Alexander Bulgakov
- */
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Открытие магазина ожидается через 2 месяца");
-    }
-}
